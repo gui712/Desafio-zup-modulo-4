c8831498650ba9f29a0da618a7a3c9d41fe5e098
Guilherme Costa <guilherme.csilva@userede.com.br>
2021-08-12 13:42:41 -0300

    iniciando projeto e-comerce

---
+++ src/main/java/br/com/userede/EcomerceRede/EcomerceRedeApplication.java
@@ -0,0 +1,13 @@
+package br.com.userede.EcomerceRede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomerceRedeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomerceRedeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/userede/EcomerceRede/EcomerceRedeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.userede.EcomerceRede;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomerceRedeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
